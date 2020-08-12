6beff470935b4db6ebf370bf2d9569e985524b4f
JIGANL <jigan.l@sfeir.com>
2020-08-12 12:09:20 +0200

    Enhance ItemActions.js

--- src/actions/ItemActions.js
+++ src/actions/ItemActions.js
@@ -1,13 +1,2 @@
-export const deleteItem = (item, action) => {
-    return {
-        type: action,
-        item
-    }
-}
-
-export const addItem = (item, action) => {
-    return {
-        type: action,
-        item
-    }
-}
+export const deleteItem = (item, DELETE_ACTION) => ({ type: DELETE_ACTION, item });
+export const addItem = (item, ADD_ACTION)  => ({ type: ADD_ACTION, item });
