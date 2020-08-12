0abde8224a3804221f3fbeb217c240d99a8a5f8d
JIGANL <jigan.l@sfeir.com>
2020-08-12 10:45:24 +0200

    Uncomment test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,32 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { mount, shallow } from 'enzyme'
+import { createMemoryHistory } from 'history'
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/rootReducer';
+import { createStore } from 'redux';
+import { Router } from "react-router-dom";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import App from './App'
+import Header from './components/Header'
+import Navigation from './components/Navigation'
+
+const store = createStore(rootReducer);
+describe('<App />', () => {
+  it('should renders without crashing', () => {
+    const wrapper = shallow(<App />)
+  });
+
+  it('shoudl renders 2 components : Header & Navigation', () => {
+    const history = createMemoryHistory();
+    const route = '/config'
+    history.push(route)
+    const wrapper = mount(
+      <Provider store={store}>
+        <Router history={history}>
+          <App />
+        </Router>
+      </Provider>)
+    expect(wrapper.contains(<Header />)).toBe(true);
+    expect(wrapper.contains(<Navigation />)).toBe(true);
+  });
+})
