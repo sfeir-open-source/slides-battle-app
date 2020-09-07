1c1c35a99cce1f4de88fcbbdcc7469a18f5953bc
JIGANL <jigan.l@sfeir.com>
2020-09-07 10:24:12 +0200

    Linter

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
-import '../App.css';
-import HomeIcon from '@material-ui/icons/Home';
+import "../App.css";
+import HomeIcon from "@material-ui/icons/Home";
 
 function Header() {
-    const backToHome = <Link to="/" className="btn btn-outline-secondary" > <HomeIcon /> </Link>
+	const backToHome = (
+		<Link to="/" className="btn btn-outline-secondary">
+			<HomeIcon />
+		</Link>
+	);
 
-    return (
-        <div className="Header">
-            <div className="col-md-1">{backToHome}</div>
-            <h1 className="col-md-11 text-center">Slide Battle</h1>
-        </div>
-    )
+	return (
+		<div className="Header">
+			<div className="col-md-1">{backToHome}</div>
+			<h1 className="col-md-11 text-center">Slide Battle</h1>
+		</div>
+	);
 }
 
 export default Header;
