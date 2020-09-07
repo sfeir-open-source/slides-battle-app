bd9f5de6440250d2eb6da260a6bd9dc6835155e2
JIGANL <jigan.l@sfeir.com>
2020-09-07 10:21:47 +0200

    Linter - Indentation

--- src/actions/ItemActions.js
+++ src/actions/ItemActions.js
@@ -1,2 +1,8 @@
-export const deleteItem = (item, DELETE_ACTION) => ({ type: DELETE_ACTION, item });
-export const addItem = (item, ADD_ACTION)  => ({ type: ADD_ACTION, item });
+export const deleteItem = (item, DELETE_ACTION) => ({
+    type: DELETE_ACTION,
+    item
+});
+export const addItem = (item, ADD_ACTION) => ({
+    type: ADD_ACTION,
+    item
+});
