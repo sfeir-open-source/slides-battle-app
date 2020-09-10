4ccd8adb7bbdd570bd529d53f8863a43da4d557e
JIGANL <jigan.l@sfeir.com>
2020-09-10 18:28:36 +0200

    update tests

--- src/components/Configuration.test.js
+++ src/components/Configuration.test.js
@@ -1,95 +1,145 @@
-import React from 'react';
-import { shallow, mount } from 'enzyme'
-import { createStore } from 'redux';
-import rootReducer from '../reducers/rootReducer';
+import React from "react";
+import { shallow, mount } from "enzyme";
+import { createStore } from "redux";
+import rootReducer from "../reducers/rootReducer";
 import { Router } from "react-router-dom";
-import { createMemoryHistory } from 'history'
-import { Provider } from 'react-redux';
-import Configuration from './Configuration'
+import { createMemoryHistory } from "history";
+import { Provider } from "react-redux";
+import Configuration from "./Configuration";
 
 const store = createStore(rootReducer);
 
-describe('<Configuration />', () => {
-    it('should have 2 components ListItems', (done) => {
-        const history = createMemoryHistory();
-        const route = '/config'
-        history.push(route)
-        const wrapper = mount(
-            <Provider store={store}>
-                <Router history={history}>
-                    <Configuration />
-                </Router>
-            </Provider>);
-        expect(wrapper.find('Connect(ListItems)').length).toBe(2);
-        done();
-    })
+const TOPICS = [
+	{
+		id: "1",
+		label: "Histoire",
+		available: false,
+	},
+	{
+		id: "2",
+		label: "Cinéma",
+		available: false,
+	},
+	{
+		id: "3",
+		label: "Bande dessiné",
+		available: false,
+	},
+	{
+		id: "4",
+		label: "Séries US",
+		available: false,
+	},
+];
+const PLAYERS = [
+	{
+		id: "1",
+		label: "Batman",
+		available: false,
+	},
+	{
+		id: "2",
+		label: "Catwoman",
+		available: false,
+	},
+	{
+		id: "3",
+		label: "Superman",
+		available: false,
+	},
+	{
+		id: "4",
+		label: "Supergirl",
+		available: false,
+	},
+];
 
-    it("should have a component ListItems with prop('type') = 'topics'", (done) => {
-        const history = createMemoryHistory();
-        const route = '/config'
-        history.push(route)
-        const wrapper = mount(
-            <Provider store={store}>
-                <Router history={history}>
-                    <Configuration />
-                </Router>
-            </Provider>);
-            const received = wrapper.find('Connect(ListItems)').get(0).props.type;
-            expect(received).toEqual('topics')
-        done();
-    })
+describe("<Configuration />", () => {
+	it("should have 2 components ListItems", (done) => {
+		const history = createMemoryHistory();
+		const route = "/config";
+		history.push(route);
+		const wrapper = mount(
+			<Provider store={store}>
+				<Router history={history}>
+					<Configuration />
+				</Router>
+			</Provider>
+		);
+		expect(wrapper.find("ListItems").length).toBe(2);
+		done();
+	});
 
-    it("should have a component ListItems with prop('type') = 'players'", (done) => {
-        const history = createMemoryHistory();
-        const route = '/config'
-        history.push(route)
-        const wrapper = mount(
-            <Provider store={store}>
-                <Router history={history}>
-                    <Configuration />
-                </Router>
-            </Provider>);
+	it("should have a component ListItems with prop('type') = 'topics'", (done) => {
+		const history = createMemoryHistory();
+		const route = "/config";
+		history.push(route);
+		const wrapper = mount(
+			<Provider store={store}>
+				<Router history={history}>
+					<Configuration />
+				</Router>
+			</Provider>
+		);
+		const received = wrapper.find("ListItems").get(0).props.type;
+		expect(received).toEqual("topics");
+		done();
+	});
 
-        const received = wrapper.find('Connect(ListItems)').get(1).props.type;
-        expect(received).toEqual('players')
-        done();
-    })
+	it("should have a component ListItems with prop('type') = 'players'", (done) => {
+		const history = createMemoryHistory();
+		const route = "/config";
+		history.push(route);
+		const wrapper = mount(
+			<Provider store={store}>
+				<Router history={history}>
+					<Configuration />
+				</Router>
+			</Provider>
+		);
+
+		const received = wrapper.find("ListItems").get(1).props.type;
+		expect(received).toEqual("players");
+		done();
+	});
 });
 
-describe('<ListItems />', () => {
-    it("should have a component ListItems with data : ['Batman', 'Catwoman', 'Superman', 'Supergirl']", (done) => {
-        const history = createMemoryHistory();
-        const route = '/config'
-        history.push(route)
-        const wrapper = mount(
-            <Provider store={store}>
-                <Router history={history}>
-                    <Configuration />
-                </Router>
-            </Provider>);
-        const received = wrapper.find('Connect(ListItems)').get(1).props.items;
-        const expected = ['Batman', 'Catwoman', 'Superman', 'Supergirl'];
-        expect(JSON.stringify(received)).toEqual(JSON.stringify(expected));
-        done();
-    })
+describe("<ListItems />", () => {
+	it("should have a component ListItems with data", (done) => {
+		const history = createMemoryHistory();
+		const route = "/config";
+		history.push(route);
+		const wrapper = mount(
+			<Provider store={store}>
+				<Router history={history}>
+					<Configuration />
+				</Router>
+			</Provider>
+		);
+		const received = wrapper.find("ListItems").get(1).props.items;
+		const expected = PLAYERS;
+		expect(JSON.stringify(received)).toEqual(JSON.stringify(expected));
+		done();
+	});
 });
 
-describe('<ListItems />', () => {
-    it("should have a component ListItems with data : ['Histoire', 'Cinéma', 'Bande dessiné', 'Series US']", (done) => {
-        const history = createMemoryHistory();
-        const route = '/config'
-        history.push(route)
-        const wrapper = mount(
-            <Provider store={store}>
-                <Router history={history}>
-                    <Configuration />
-                </Router>
-            </Provider>);
-        const received = wrapper.find('Connect(ListItems)').get(0).props.items;
-        const expected = ['Histoire', 'Cinéma', 'Bande dessiné', 'Series US'];
-        expect(JSON.stringify(received)).toEqual(JSON.stringify(expected));
-        done();
-    })
+describe("<ListItems />", () => {
+	it("should have a component ListItems with data", (done) => {
+		const history = createMemoryHistory();
+		const route = "/config";
+		history.push(route);
+		const wrapper = mount(
+			<Provider store={store}>
+				<Router history={history}>
+					<Configuration />
+				</Router>
+			</Provider>
+		);
+		const received = wrapper.find("ListItems").get(0).props.items;
+		const expected = TOPICS;
+		expect(JSON.stringify(received)).toEqual(JSON.stringify(expected));
+		done();
+	});
 });
 /*
 describe('<Configuration />', () => {
